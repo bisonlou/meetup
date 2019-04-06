15bc678f8fce87c81acbf7a0e62a921bff88bd80
innocent lou <bisonlou@gmail.com>
2019-04-06 19:20:27 +0300

    Removed search imports

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { Navbar } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
 
 class Header extends Component {
     render() {
