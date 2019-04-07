76979ead881aedfe16bfebb0b4f0f0b957270f5a
innocent lou <bisonlou@gmail.com>
2019-04-07 23:30:24 +0300

    Added meeting list

--- src/meetups/meetups.js
+++ src/meetups/meetups.js
@@ -3,6 +3,7 @@ import Header from '../components/header'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import MeetupSummaryPanel from './meetup_summary_panel'
+import MeetingList from './meeting_list'
 import './style.css'
 
 class Meetups extends Component {
@@ -15,6 +16,7 @@ class Meetups extends Component {
 
                     <Col sm={6} className="middle-panel">
                         <MeetupSummaryPanel />
+                        <MeetingList />
                     </Col>
 
                     <Col sm={3}></Col>
